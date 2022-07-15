bd0b49830bca0b1331f99c500418828ebe184edb
Suraj Kumar <ravi.suraj110@gmail.com>
2022-07-15 16:23:49 +0530

    add image upload for registration

--- lib/fkc_elixir_web/live/register_live.ex
+++ lib/fkc_elixir_web/live/register_live.ex
@@ -6,6 +6,16 @@ defmodule FkcElixirWeb.RegisterLive do
 
   def mount(_params, _session, socket) do
     changeset = Accounts.change_user_registration(%User{})
+
+    socket =
+      allow_upload(
+        socket,
+        :image,
+        accept: ~w(.png .jpg .jpeg),
+        max: 3,
+        max_file_size: 5_000_000
+      )
+
     {:ok, assign(socket, changeset: changeset, trigger_submit: false)}
   end
 
@@ -18,6 +28,10 @@ defmodule FkcElixirWeb.RegisterLive do
   def handle_event("validate", %{"user" => params}, socket) do
     changeset = registration_changeset(params)
     {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("cancel", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :image, ref)}
   end
 
   defp registration_changeset(params) do
